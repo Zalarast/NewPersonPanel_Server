ade8da9c756375eca473930f6cdd52efb03bc73d
Zalarast <shubin.bodya@gmail.com>
2022-07-20 16:09:16 +0300

    Изменил на Ассинхронную функцию

--- SQL/models/index.ts
+++ SQL/models/index.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize/types";
 import { DataTypes } from "sequelize";
 
-export function models (sequelize: Sequelize) {
+export async function models (sequelize: Sequelize) {
     sequelize.define("Users", {
         id: { type: DataTypes.INTEGER, autoIncrement: true, allowNull: false, primaryKey: true },
         login: { type: DataTypes.STRING },
