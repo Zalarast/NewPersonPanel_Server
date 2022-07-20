2abb834f8daacf083b8124c7e36226528d5a3d7f
Zalarast <shubin.bodya@gmail.com>
2022-07-20 09:29:59 +0300

    Сделал обработчик запроса на аутентификацию

--- server.ts
+++ server.ts
@@ -1,16 +1,21 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors';
 import { AuthBody } from './types'
+import { initDB } from './SQL/SQL';
 
-const server = fastify({logger:true})
+const server = fastify({logger:true}).register(cors);
 
 server.post<{Body: AuthBody}>('/auth', async (req, res) => {
-    req.body
+  const {login, password} = req.body;
+  if (login == "1" && password == "2")
+    return {result: true}
 })
 
-server.listen({host: "0.0.0.0", port: 8080 }, (err, address) => {
+server.listen({host: "0.0.0.0", port: 8080 }, async (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
   }
   console.log(`Server listening at ${address}`)
+  await initDB();
 })
