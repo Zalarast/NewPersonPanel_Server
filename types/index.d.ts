9cccef6e74d7fe65f3b3ecfa5fcad63cee454ebf
Zalarast <shubin.bodya@gmail.com>
2022-07-20 09:30:45 +0300

    Интерфейс для модели sequelize

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,3 +2,10 @@ export interface AuthBody {
     login: string;
     password: string;
 }
+
+export interface UsersModel {
+    id: number;
+    login: string;
+    password: string;
+    token: string;
+}
