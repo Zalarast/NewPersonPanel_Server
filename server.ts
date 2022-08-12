429cfd41b113bbc14cb3231425c402a09ffc697e
Zalarast <shubin.bodya@gmail.com>
2022-08-12 16:04:51 +0300

    Ссылка/путь info

--- server.ts
+++ server.ts
@@ -1,12 +1,16 @@
 import fastify from 'fastify'
 import cors from '@fastify/cors';
-import { AuthBody } from './types'
-import { initDB, authUser } from './SQL/SQL';
+import { AuthBody, InfoBody } from './types'
+import { initDB, authUser, getInfo } from './SQL/SQL';
 
 const server = fastify({logger:true}).register(cors);
 
 server.post<{Body: AuthBody}>('/auth', async (req, res) => {
   return await authUser(req.body);
+})
+
+server.post<{Body: InfoBody}>("/getInfo", async (req, res)=> {
+  return await getInfo(req.body)
 })
 
 server.listen({host: "0.0.0.0", port: 8080 }, async (err, address) => {
